dd6c6660a75002ddba6d45095c37da1caee69d8e
Roland <rbell13@gmu.edu>
2022-03-17 00:41:27 -0400

    saves files locally from github, retrieves yaml files

--- go.mod
+++ go.mod
@@ -3,16 +3,15 @@ module github.com/rbell13/oa-sis
 go 1.17
 
 require (
-	github.com/Masterminds/vcs v1.13.1 // indirect
+	github.com/Masterminds/vcs v1.13.1
 	github.com/davecgh/go-spew v1.1.1
-	github.com/deepmap/oapi-codegen v1.9.1 // indirect
-	github.com/getkin/kin-openapi v0.92.0 // indirect
+	github.com/deepmap/oapi-codegen v1.9.1
+	github.com/getkin/kin-openapi v0.92.0
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.5 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/echo/v4 v4.7.1 // indirect
+	github.com/labstack/echo/v4 v4.7.1
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
@@ -20,12 +19,9 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220313003712-b769efc7c000 // indirect
-	golang.org/x/mod v0.4.1 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-	golang.org/x/tools v0.0.0-20210114065538-d78b04bdf963 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,28 +1,35 @@
 package service
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
+	"github.com/Masterminds/vcs"
 	"github.com/davecgh/go-spew/spew"
 	echoV4 "github.com/labstack/echo/v4"
-
-	"github.com/Masterminds/vcs"
 	"github.com/rbell13/oa-sis/pkg/gen/OAsis"
 )
 
 const (
-	REPOS_ENV      = "REPOS"
-	FORMAT_ENV     = "FORMAT"
-	FORMAT_DEFAULT = ".*-oas.yaml"
+	REPOS_ENV       = "REPOS"
+	FORMAT_ENV      = "FORMAT"
+	FORMAT_DEFAULT  = ".*-oas.yaml"
+	REFRESH_SECONDS = 60
+	RepoNotFound    = "Repo not found"
 )
 
 type OAsisService struct {
 	repos        []string
 	formatString string
+	oasFiles     []string
 }
 
 func NewOAsisService() (oas *OAsisService) {
@@ -31,11 +38,11 @@ func NewOAsisService() (oas *OAsisService) {
 		reposList string
 	)
 
-	if format = os.Getenv(FORMAT_ENV); format != "" {
+	if format = os.Getenv(FORMAT_ENV); format == "" {
 		format = FORMAT_DEFAULT
 	}
 
-	if reposList = os.Getenv(REPOS_ENV); reposList != "" {
+	if reposList = os.Getenv(REPOS_ENV); reposList == "" {
 		panic("No repos?")
 	}
 
@@ -46,49 +53,101 @@ func NewOAsisService() (oas *OAsisService) {
 
 	go func() {
 		for _, repo := range oas.repos {
-			oas.updateRepo(repo, oas.formatString)
-			time.Sleep(5)
+			var err error
+
+			oas.oasFiles, err = oas.updateRepo(repo, oas.formatString)
+			if err != nil {
+				panic(err)
+			}
+
+			getOASFromRemote(context.Background(), repo, oas.oasFiles)
 		}
+		
+		time.Sleep(time.Second * REFRESH_SECONDS)
 	}()
 
 	return
 }
 
-// (GET /index)
+// (GET /index).
 func (oasis *OAsisService) GetIndex(ctx echoV4.Context) error {
 	return echoV4.NewHTTPError(http.StatusNotImplemented)
 }
 
-// (GET /json/{spec})
+// (GET /json/{spec}).
 func (oasis *OAsisService) GetJsonSpec(ctx echoV4.Context, spec OAsis.Spec) error {
 	return echoV4.NewHTTPError(http.StatusNotImplemented)
 }
 
-// (GET /yaml/{spec})
+// (GET /yaml/{spec}).
 func (oasis *OAsisService) GetYamlSpec(ctx echoV4.Context, spec OAsis.Spec) error {
-
-	return echoV4.NewHTTPError(http.StatusNotImplemented)
+	return ctx.File("/tmp/" + string(spec) + ".yaml")
 }
 
-// updateRepo scans a github repository for filenames that match a given format string and returns a list of filenames
-func (oasis *OAsisService) updateRepo(repo, format string) (files string, err error) {
+// updateRepo scans a github repository for filenames that match a given format string and returns a list of filenames.
+func (oasis *OAsisService) updateRepo(repo, format string) (files []string, err error) {
 	svn, err := vcs.NewSvnRepo(repo, "")
 	if err != nil {
 		return
 	}
 
 	if !svn.Ping() {
-		err = errors.New("repo not found")
-		return
+		return nil, errors.New(RepoNotFound)
 	}
 
-	lsBytes, err := svn.RunFromDir("svn", "ls", "--verbose", "--recursive", "--depth=infinity", ".")
+	lsBytes, err := svn.RunFromDir("svn", "ls", "-R", "--recursive", "--depth=infinity", repo)
 	if err != nil {
 		return
 	}
 
-	files = string(lsBytes)
-	spew.Dump(files)
+	files = regexp.MustCompile(format).FindStringSubmatch(string(lsBytes))
 
 	return
+}
+
+func getOASFromRemote(ctx context.Context, repo string, files []string) {
+	repourl, err := url.Parse(repo)
+	if err != nil {
+		spew.Dump(err)
+	}
+
+	for _, file := range files {
+		rawURL := fmt.Sprintf("https://raw.githubusercontent.com" + repourl.Path + "/main/" + strings.Trim(file, "trunk/"))
+		spew.Dump(rawURL)
+
+		req, err := http.NewRequestWithContext(ctx, "GET", rawURL, nil)
+		if err != nil {
+			spew.Dump(err)
+
+			continue
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			spew.Dump(err)
+
+			continue
+		}
+		defer resp.Body.Close()
+
+		err = os.Mkdir("/tmp", os.ModeDir)
+		if err != nil {
+			spew.Dump(err)
+
+			continue
+		}
+
+		localfile, err := os.Create("/tmp/" + strings.ReplaceAll(file, "trunk/pkg/", ""))
+		if err != nil {
+			spew.Dump(err)
+		}
+		defer localfile.Close()
+
+		size, err := io.Copy(localfile, resp.Body)
+		if err != nil {
+			spew.Dump(err)
+		}
+
+		spew.Dump(size)
+	}
 }
